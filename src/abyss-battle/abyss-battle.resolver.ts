411063cd20119d1b3042025609d16e0298b0d8a2
Bouhm <bbouhm@gmail.com>
2021-06-03 00:08:07 -0700

    idk

--- src/abyss-battle/abyss-battle.resolver.ts
+++ src/abyss-battle/abyss-battle.resolver.ts
@@ -1,4 +1,6 @@
+import { Artifact } from 'src/artifact/artifact.model';
 import { PlayerCharacter } from 'src/player-character/player-character.model';
+import { Weapon } from 'src/weapon/weapon.model';
 
 import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 
@@ -15,12 +17,25 @@ export class AbyssBattleResolver {
     return this.abyssBattleService.list(filter);
   }
 
-  @ResolveField()
-  async parties(@Parent() abyssBattle: AbyssBattleDocument) {
+  @ResolveField(() => [[PlayerCharacter]])
+  async characters(@Parent() abyssBattle: AbyssBattleDocument) {
     const battle = await abyssBattle
-      .populate({ path: 'parties', model: PlayerCharacter.name })
+      .populate({
+        path: 'parties',
+        model: PlayerCharacter.name,
+        populate: [
+          {
+            path: 'artifacts',
+            model: Artifact.name,
+          },
+          {
+            path: 'weapon',
+            model: Weapon.name,
+          },
+        ],
+      })
       .execPopulate();
-
+    // console.log(battle.parties);
     return battle.parties;
   }
 }
