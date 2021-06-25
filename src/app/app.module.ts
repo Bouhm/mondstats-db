4204b132099b1b26bf4ed44eb4d2a918897abfa9
Bouhm <bbouhm@gmail.com>
2021-06-24 19:30:02 -0700

    introspection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWOR
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       playground: true,
+      introspection: true,
       path: '/dev/playground',
       debug: false,
     }),
