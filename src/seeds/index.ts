c976de72cee844255f998a71ffa88c4bf119340b
Bouhm <bbouhm@gmail.com>
2021-07-06 15:10:20 -0700

    nvm set threshold to 9

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -241,7 +241,7 @@ const getSpiralAbyssData = async (
   server: string,
   uid: number,
   scheduleType = 1,
-  threshold = 11,
+  threshold = 9,
   secondTry = false,
 ) => {
   const apiUrl = `${spiralAbyssApiUrl}?server=os_${server}&role_id=${uid}&schedule_type=${scheduleType}`;
